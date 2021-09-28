08e9dafcd8b6b543de81014e105d8aa8bb3c3277
cmacdougall12 <cammacdougall12@gmail.com>
2021-09-28 13:33:24 -0600

    contact component and added contact context

---
+++ client/src/components/NewContactModal.js
@@ -0,0 +1,40 @@
+import React, { useRef } from "react";
+import { Modal, Form, Button } from "react-bootstrap";
+import { useContacts} from "../context/ContactsProvider";
+import { useConversations} from "../context/ConversationsProvider";
+
+export default function NewContactModal({ closeModal }) {
+  const idRef = useRef();
+  const nameRef = useRef();
+  const { createContact } = useContacts();
+  const { createConversation} = useConversations
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    createContact(idRef.current.value, nameRef.current.value);
+
+    closeModal();
+  };
+
+  return (
+    <>
+      <Modal.Header closeButton>Create Contact</Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>Id</Form.Label>
+            <Form.Control type="text" ref={idRef}></Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" ref={nameRef}></Form.Control>
+          </Form.Group>
+          <Button type="submit" className="mt-2">
+            Create
+          </Button>
+        </Form>
+      </Modal.Body>
+    </>
+  );
+}
